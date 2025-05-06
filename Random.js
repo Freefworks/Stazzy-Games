556c6cb8f756292e1108b4408fe20f2e75aac731
Stazzy <freef@tutamail.com>
2025-05-06 01:23:01 -0400

    Koopa

--- Random.js
+++ Random.js
@@ -18,5 +18,6 @@ var sites = [
 'game-17.html',
 'game-18.html',
 'game-19.html',
-'game-20.html'
+'game-20.html',
+'game-21.html'
 ]; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
