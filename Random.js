6eb2f8e0e4528a3a9a94b9bf80dbba29c5e98e4f
Stazzy <freef@tutamail.com>
2026-03-21 23:39:21 -0400

    Cool Slide Bro

--- Random.js
+++ Random.js
@@ -58,5 +58,6 @@ var sites = [
 'game-57.html',
 'game-58.html',
 'game-59.html',
-'game-60.html'
+'game-60.html',
+'game-61.html'
 ]; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
